7666d9864169b804d22452df97a630b8b1d0a587
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-07-09 11:08:15 -0500

    Time: 51 ms (92.33%) | Memory: 17.8 MB (10.84%) - LeetSync

--- Trees/1-two-sum/two-sum.py
+++ Trees/1-two-sum/two-sum.py
@@ -1,35 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         lst = []
-#         flag = False
-#         for i in nums:
-#             for j in nums:
-#                 print(i)
-#                 print(j)
-#                 print(nums.index(i))
-#                 print(nums.index(j))
-#                 print(i+j == target and nums.index(i) != nums.index(j))
-#                 if i+j == target and nums.index(i) != nums.index(j):
-#                     lst.append(nums.index(i))
-#                     lst.append(nums.index(j))
-#                     flag = True
-                    
-#                     print(nums.index(i))
-#                     print(nums.index(j))
-#                     break
-#             if flag:
-#                 break
-        
-#         #     if len(lst) > 1:
-#         #         break
-#         print(lst)
-#         return lst
+        hm = {}
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val in hm:
+                return [i, hm[val]]
+            else:
+                hm[nums[i]] = i 
