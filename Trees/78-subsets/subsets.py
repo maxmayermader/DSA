5c53962b724613cbeecfd29f70e1eed204a5a4cf
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-07-11 13:59:52 -0500

    Time: 35 ms (72.79%) | Memory: 16.7 MB (65.77%) - LeetSync

--- Trees/78-subsets/subsets.py
+++ Trees/78-subsets/subsets.py
@@ -1,19 +1,36 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # res = []
-        def backtrack(first=0, curr=[]):
-            if len(curr) == k:
-                res.append(curr[:])
-                return
-            for i in range(first, n):
-                curr.append(nums[i])
-                backtrack(i+1, curr)
-                curr.pop()
-
         res = []
-        n = len(nums)
-        for k in range(n+1):
-            backtrack()
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
         return res
+        # # res = []
+        # def backtrack(first=0, curr=[]):
+        #     if len(curr) == k:
+        #         res.append(curr[:])
+        #         return
+        #     for i in range(first, n):
+        #         curr.append(nums[i])
+        #         backtrack(i+1, curr)
+        #         curr.pop()
+
+        # res = []
+        # n = len(nums)
+        # for k in range(n+1):
+        #     backtrack()
+        # return res
 
         
