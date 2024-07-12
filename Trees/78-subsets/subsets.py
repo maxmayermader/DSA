0ce106336d7d8c1bd575ccf4ffd00b9e60191f8c
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-07-12 14:48:00 -0500

    Time: 27 ms (97.64%) | Memory: 16.8 MB (24.95%) - LeetSync

--- Trees/78-subsets/subsets.py
+++ Trees/78-subsets/subsets.py
@@ -1,34 +1,17 @@
-# class Solution:
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         res = []
-#         subset= []
-#         def dfs(i):
-#             if i >= len(nums):
-#                 res.append(subset.copy())
-#                 return
-#             subset.append(nums[i])
-#             dfs(i+1)
-
-#             subset.pop()
-#             dfs(i+1)
-
-
-#         dfs(0)
-#         return res
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        curr = []
-        
-        def bt(i):
-            nonlocal curr
-            if i == len(nums):
-                yield copy.deepcopy(curr)
+        res = []
+        subset= []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
                 return
-            curr.append(nums[i]) 
-            yield from bt(i + 1)
-            curr.pop()
-            yield from bt(i + 1)
-        return list(bt(0))            
+            subset.append(nums[i])
+            dfs(i+1)
+
+            subset.pop()
+            dfs(i+1)
 
 
+        dfs(0)
+        return res
