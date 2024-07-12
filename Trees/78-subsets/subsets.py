1452fd671d07f557bbb063f1365c66abfdf3095d
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-07-12 14:46:57 -0500

    Time: 35 ms (72.74%) | Memory: 16.8 MB (24.95%) - LeetSync

--- Trees/78-subsets/subsets.py
+++ Trees/78-subsets/subsets.py
@@ -1,36 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-
-        subset = []
-
+        subset= []
         def dfs(i):
             if i >= len(nums):
                 res.append(subset.copy())
                 return
-            # decision to include nums[i]
             subset.append(nums[i])
-            dfs(i + 1)
-            # decision NOT to include nums[i]
+            dfs(i+1)
+
             subset.pop()
-            dfs(i + 1)
+            dfs(i+1)
+
 
         dfs(0)
         return res
-        # # res = []
-        # def backtrack(first=0, curr=[]):
-        #     if len(curr) == k:
-        #         res.append(curr[:])
-        #         return
-        #     for i in range(first, n):
-        #         curr.append(nums[i])
-        #         backtrack(i+1, curr)
-        #         curr.pop()
-
-        # res = []
-        # n = len(nums)
-        # for k in range(n+1):
-        #     backtrack()
-        # return res
-
-        
