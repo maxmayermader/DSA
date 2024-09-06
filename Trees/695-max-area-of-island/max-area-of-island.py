b1a3a00190dd0b17531e415cb23e0ccaeba2035f
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-09-06 17:01:30 -0500

    Time: 109 ms (57.45%) | Memory: 16.9 MB (88.58%) - LeetSync

---
+++ Trees/695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        res = 0
+        visited = set()
+
+        def bfs(start):
+            q = deque()
+            q.append(start)
+            visited.add(start)
+            islandSize = 1
+
+            while q:
+                r, c = q.popleft()
+                adj = []
+                
+                for i in range(-1,2):
+                    if i != 0 and r + i >= 0 and r + i < ROWS:
+                        adj.append((r+i, c))
+                for j in range(-1,2):
+                    if j != 0 and c + j >= 0 and c + j < COLS:
+                        adj.append((r, c+j))
+
+                for nr, nc in adj:
+                    if grid[nr][nc] != 0 and (nr, nc) not in visited:
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+                        islandSize += 1
+               
+
+            return islandSize
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] != 0 and (i,j) not in visited:
+                    res = max(res, bfs((i,j)))
+
+        return res
+                        
+
+            
