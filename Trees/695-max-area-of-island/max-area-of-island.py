123235b780786626c57fc19701614a38cf1e938f
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-09-06 17:02:13 -0500

    Time: 105 ms (70.11%) | Memory: 17 MB (80.76%) - LeetSync

--- Trees/695-max-area-of-island/max-area-of-island.py
+++ Trees/695-max-area-of-island/max-area-of-island.py
@@ -14,15 +14,24 @@ class Solution:
                 r, c = q.popleft()
                 adj = []
                 
-                for i in range(-1,2):
-                    if i != 0 and r + i >= 0 and r + i < ROWS:
-                        adj.append((r+i, c))
-                for j in range(-1,2):
-                    if j != 0 and c + j >= 0 and c + j < COLS:
-                        adj.append((r, c+j))
+                # for i in range(-1,2):
+                #     if i != 0 and r + i >= 0 and r + i < ROWS:
+                #         adj.append((r+i, c))
+                # for j in range(-1,2):
+                #     if j != 0 and c + j >= 0 and c + j < COLS:
+                #         adj.append((r, c+j))
 
-                for nr, nc in adj:
-                    if grid[nr][nc] != 0 and (nr, nc) not in visited:
+                # for nr, nc in adj:
+                #     if grid[nr][nc] != 0 and (nr, nc) not in visited:
+                #         q.append((nr, nc))
+                #         visited.add((nr, nc))
+                #         islandSize += 1
+                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # 4-directional movement
+
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < ROWS and 0 <= nc < COLS and 
+                        grid[nr][nc] == 1 and (nr, nc) not in visited):
                         q.append((nr, nc))
                         visited.add((nr, nc))
                         islandSize += 1
