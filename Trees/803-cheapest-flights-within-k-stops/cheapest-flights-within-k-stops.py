001182c46140c20f8194fe58e53b3b0ccbabd910
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-09-28 16:25:25 -0500

    Time: 92 ms (80.10%) | Memory: 17.9 MB (60.53%) - LeetSync

--- Trees/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ Trees/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,66 +1,24 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adj = {i: [] for i in range(n)}
-        for frm, to, price in flights:
-            adj[frm].append((to, price))
-        
-        pq = [(0, src, k + 1)]
-        visited = {}
-        
+        adj = {i:[] for i in range(n)}
+        for node, nei, cost in flights:
+            adj[node].append((cost, nei))
+
+        pq = [(0, src, k+1)]
+        visit = {}
+        # totStops = k
+
         while pq:
-            price, node, stops = heapq.heappop(pq)
-            
+            cost, node, stops = heapq.heappop(pq)
+
             if node == dst:
-                return price
-            
+                return cost
+
             if stops > 0:
-                if node not in visited or visited[node] < stops:
-                    visited[node] = stops
-                    for nei, cost in adj[node]:
-                        heapq.heappush(pq, (price + cost, nei, stops - 1))
-        
+                if node not in visit or visit[node] < stops:
+                    visit[node] = stops
+                    for neiCost, nei in adj[node]:
+                        heapq.heappush(pq, (cost+neiCost, nei, stops-1))
+
         return -1
 
-
-# class Solution:
-#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         res = 0
-#         adj = {i:[] for i in range(n)}
-#         resArr = [[float('inf') for j in range(k+2)] for i in range(n)]
-#         for i, edge, cost in flights:
-#             adj[i].append([edge, cost])
-
-#         pq = []
-#         heapq.heappush(pq, [0, src, k+1])
-#         print(resArr)
-#         while pq:
-#             dist, u, stops = heapq.heappop(pq)
-
-#             if u == dst:
-#                 return dist
-            
-#             if stops > 0:
-#                 for nei, cost in adj[u]:
-#                     newDist = dist + cost
-#                     if resArr[nei][stops -1] > newDist:
-#                         heapq.heappush(pq, [newDist, nei, stops-1])
-            
-
-
-#         return -1
-
-#         # visit = set()
-#         # q = deque()
-#         # q.append(adj[0])
-#         # lvl = 0
-        
-#         # while q:
-#         #     for i in range(len(q)):
-#         #         node = q.popleft()
-#         #         print(node)
-            
-#         #     lvl += 1
-
-#         # print(resArr)
-#         # return res
-        
