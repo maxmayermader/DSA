567de13b481a9336584af3810e7d80d8efaf546c
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-05 22:25:46 -0500

    Time: 423 ms (46.62%) | Memory: 34.6 MB (5.28%) - LeetSync

--- Trees/217-contains-duplicate/contains-duplicate.py
+++ Trees/217-contains-duplicate/contains-duplicate.py
@@ -1,3 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        # return len(nums) != len(set(nums))
+        hm = {}
+        for num in nums:
+            if num in hm:
+                return True
+            hm[num] = 1 + hm.get(num,0)
+
+
+        return False
