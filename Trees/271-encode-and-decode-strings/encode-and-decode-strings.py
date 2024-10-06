c8bc9f2b0c2641ad290292e1cadaf57d88b5338a
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-06 16:03:26 -0500

    Time: 63 ms (79.67%) | Memory: 16.8 MB (85.07%) - LeetSync

--- Trees/271-encode-and-decode-strings/encode-and-decode-strings.py
+++ Trees/271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -1,40 +1,53 @@
 class Codec:
     def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
-        res = ""
-        # for word in strs:
-        #     res += chr(300) + word + chr(299)
-        for i in range(len(strs)):
-            if i != len(strs)-1:
-                res += strs[i] + chr(300)
-            else:
-                res += strs[i]
+        return ''.join(f'{len(s)}:{s}' for s in strs)
 
-        print(res)
+    def decode(self, s: str) -> List[str]:
+        result, i = [], 0
+        while i < len(s):
+            j = s.find(':', i)
+            length = int(s[i:j])
+            result.append(s[j+1:j+1+length])
+            i = j + 1 + length
+        return result
 
-        return res
+# class Codec:
+#     def encode(self, strs: List[str]) -> str:
+#         """Encodes a list of strings to a single string.
+#         """
+#         res = ""
+#         # for word in strs:
+#         #     res += chr(300) + word + chr(299)
+#         for i in range(len(strs)):
+#             if i != len(strs)-1:
+#                 res += strs[i] + chr(300)
+#             else:
+#                 res += strs[i]
+
+#         print(res)
+
+#         return res
             
 
         
 
-    def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
-        """
-        res = []
+#     def decode(self, s: str) -> List[str]:
+#         """Decodes a single string to a list of strings.
+#         """
+#         res = []
 
-        # for c in s:
-        #     word = ""
-            # while c != char()
+#         # for c in s:
+#         #     word = ""
+#             # while c != char()
 
-        res = s.split(chr(300))
-        print(res)
+#         res = s.split(chr(300))
+#         print(res)
 
-        return res
+#         return res
         
         
 
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+# # Your Codec object will be instantiated and called as such:
+# # codec = Codec()
+# # codec.decode(codec.encode(strs))
