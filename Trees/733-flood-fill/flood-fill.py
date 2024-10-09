d20799c73d814278a94d7df01a83129ebfc72a6a
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-09 15:25:02 -0500

    Time: 74 ms (12.01%) | Memory: 16.8 MB (47.33%) - LeetSync

---
+++ Trees/733-flood-fill/flood-fill.py
@@ -0,0 +1,23 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        q = deque()
+        prev = image[sr][sc]
+        q.append((sr, sc))
+        visit = set()
+        # visit.add((sr, sc))
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while q:
+            r, c = q.popleft()
+            if (r,c) in visit:
+                continue
+            visit.add((r, c))
+            image[r][c] = color
+            for dr, dc in directions:
+                newR, newC = r+dr, c+dc
+                if (newR, newC) not in visit and 0 <= newR < len(image) and 0 <= newC < len(image[0]) and image[newR][newC] == prev:
+                    q.append((newR, newC))
+                    
+
+        return image
+            
