539f4c1d1ca02672b022c7895f5c464e05a7facf
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-08 21:23:27 -0500

    Time: 92 ms (56.50%) | Memory: 16.6 MB (18.84%) - LeetSync

--- Trees/36-valid-sudoku/valid-sudoku.py
+++ Trees/36-valid-sudoku/valid-sudoku.py
@@ -1,19 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = defaultdict(set)
-        col = defaultdict(set)
-        grid = defaultdict(set)
-
-        print(row)
-
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        diags = defaultdict(set)
+        
         for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == '.':
-                    continue
-                elif (board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in grid[i // 3, j // 3]):
-                    return False
-                else:
-                    row[i].add(board[i][j])
-                    col[j].add(board[i][j])
-                    grid[i//3, j//3].add(board[i][j])
+            for j in range(len(board)):
+                if board[i][j] != '.':
+                    if (board[i][j] in rows[i] or
+                        board[i][j] in cols[j] or
+                        board[i][j] in diags[(i//3, j//3)]):
+                        return False
+
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    diags[((i//3), (j//3))].add(board[i][j])
+
         return True
+                    
