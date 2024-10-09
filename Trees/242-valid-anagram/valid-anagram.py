02540a9e573289807b5e54348ae9c37d74ebb57b
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-08 20:00:47 -0500

    Time: 45 ms (75.37%) | Memory: 17 MB (55.44%) - LeetSync

--- Trees/242-valid-anagram/valid-anagram.py
+++ Trees/242-valid-anagram/valid-anagram.py
@@ -3,18 +3,10 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        hm1 = {}
-        hm2 = {}
+        hm1, hm2 = {}, {}
 
-        for c in s:
-            hm1[c] = 1 + hm1.get(c, 0)
-
-        for c in t:
-            hm2[c] = 1 + hm2.get(c, 0)
-
-        for c in t:
-            if c not in hm1 or hm1[c] != hm2[c]:
-                return False
-
-        return True
-        
+        for i in range(len(s)):
+            hm1[s[i]] = 1 + hm1.get(s[i], 0)
+            hm2[t[i]] = 1 + hm2.get(t[i], 0)
+        res = hm1 == hm2
+        return res
