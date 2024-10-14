686ef289dac215af4ca9506ad03c396264982038
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-14 15:33:34 -0500

    Time: 698 ms (46.35%) | Memory: 39 MB (30.65%) - LeetSync

---
+++ Trees/883-car-fleet/car-fleet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [(position[i], speed[i]) for i in range(len(speed))]
+        cars.sort(reverse=True)
+        stack=[]
+        for p, s in cars:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
