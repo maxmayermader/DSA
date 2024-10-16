dcbe563e13a28fd350de19caf889d1c9bce2dee3
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-16 17:50:26 -0500

    Time: 325 ms (38.50%) | Memory: 53.8 MB (32.58%) - LeetSync

---
+++ Trees/836-race-car/race-car.py
@@ -0,0 +1,54 @@
+# class Solution:
+#     def racecar(self, target: int) -> int:
+#         pq = [(0, target, 0, 1, ['A']), (0, target, 0, 1, ['R'])]
+
+#         while pq:
+#             _, distToTarget, pos, speed, gears = heapq.heappop(pq)
+
+#             if gears[-1] == 'A':
+#                 pos = pos + speed
+#                 speed*=2
+#             else:
+#                 if speed > 0:
+#                     speed = -1
+#                 else:
+#                     speed = 1
+
+#             if pos == target:
+#                 return len(gears)
+#             agears = gears.copy()
+#             agears.append('A')
+#             rgears = gears.copy()
+#             rgears.append('R')
+#             # print(agears, rgears)
+#             heapq.heappush(pq, (len(agears), abs(target-pos), pos, speed, agears))
+#             heapq.heappush(pq, (len(rgears), abs(target-pos), pos, speed, rgears))
+
+class Solution:
+    def racecar(self, target: int) -> int:
+        queue = deque([(0, 1)])  # (position, speed)
+        steps = 0
+        visited = set([(0, 1)])
+
+        while queue:
+            for _ in range(len(queue)):
+                pos, speed = queue.popleft()
+
+                if pos == target:
+                    return steps
+
+                # Accelerate
+                new_pos, new_speed = pos + speed, speed * 2
+                if (new_pos, new_speed) not in visited and 0 <= new_pos <= 2 * target:
+                    queue.append((new_pos, new_speed))
+                    visited.add((new_pos, new_speed))
+
+                # Reverse
+                new_speed = -1 if speed > 0 else 1
+                if (pos, new_speed) not in visited:
+                    queue.append((pos, new_speed))
+                    visited.add((pos, new_speed))
+
+            steps += 1
+
+        return -1  # If target is unreachable
