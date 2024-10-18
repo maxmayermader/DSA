b420de6ad08bb883495a195f7538a42f99c27b64
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-18 16:02:46 -0500

    Time: 36 ms (65.61%) | Memory: 16.5 MB (53.20%) - LeetSync

--- Trees/535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ Trees/535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -1,51 +1,27 @@
-# class Codec:
-#     def __init__(self):
-#         self.urls = {}
-#         self.urlNum = 0
-
-
-#     def encode(self, longUrl: str) -> str:
-#         """Encodes a URL to a shortened URL.
-#         """
-#         newUrl = "http://tinyurl.com/"+str(self.urlNum)
-#         self.urlNum+=1
-#         self.urls[newUrl] = longUrl
-#         return newUrl
-
-        
-
-#     def decode(self, shortUrl: str) -> str:
-#         """Decodes a shortened URL to its original URL.
-#         """
-#         return self.urls[shortUrl]
-
-        
-
-# # Your Codec object will be instantiated and called as such:
-# # codec = Codec()
-# # codec.decode(codec.encode(url))
 class Codec:
     def __init__(self):
-        self.encodeurl = {}
-        self.decodeurl = {}
+        self.urls = {}
+        self.urlNum = 0
+        self.base = "http://tinyurl.com/"
 
-        self.baseurl = "http://tinyurl.com/"
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        if longUrl not in self.encodeurl:
-            shortUrl = self.baseurl + str(len(self.encodeurl))
-            self.encodeurl[longUrl] = shortUrl 
-            self.decodeurl[shortUrl] = longUrl
-        return self.encodeurl[longUrl]
+        # newUrl = "http://tinyurl.com/"+str(self.urlNum)
+        newUrl = self.base+str(self.urlNum)
+        self.urls[newUrl] = longUrl
+        self.urlNum+=1
+        return newUrl
 
         
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return self.decodeurl[shortUrl]
+        return self.urls[shortUrl]
+
+        
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
