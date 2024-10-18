2051b34cbd05f4b7947308a2fb3846b2ce765c58
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-18 16:00:37 -0500

    Time: 24 ms (99.11%) | Memory: 16.7 MB (10.32%) - LeetSync

--- Trees/535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ Trees/535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -1,25 +1,51 @@
+# class Codec:
+#     def __init__(self):
+#         self.urls = {}
+#         self.urlNum = 0
+
+
+#     def encode(self, longUrl: str) -> str:
+#         """Encodes a URL to a shortened URL.
+#         """
+#         newUrl = "http://tinyurl.com/"+str(self.urlNum)
+#         self.urlNum+=1
+#         self.urls[newUrl] = longUrl
+#         return newUrl
+
+        
+
+#     def decode(self, shortUrl: str) -> str:
+#         """Decodes a shortened URL to its original URL.
+#         """
+#         return self.urls[shortUrl]
+
+        
+
+# # Your Codec object will be instantiated and called as such:
+# # codec = Codec()
+# # codec.decode(codec.encode(url))
 class Codec:
     def __init__(self):
-        self.urls = {}
-        self.urlNum = 0
+        self.encodeurl = {}
+        self.decodeurl = {}
 
+        self.baseurl = "http://tinyurl.com/"
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        newUrl = "http://tinyurl.com/"+str(self.urlNum)
-        self.urlNum+=1
-        self.urls[newUrl] = longUrl
-        return newUrl
+        if longUrl not in self.encodeurl:
+            shortUrl = self.baseurl + str(len(self.encodeurl))
+            self.encodeurl[longUrl] = shortUrl 
+            self.decodeurl[shortUrl] = longUrl
+        return self.encodeurl[longUrl]
 
         
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return self.urls[shortUrl]
-
-        
+        return self.decodeurl[shortUrl]
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
