cc2441a8bb63407bb62dae59974ef205b720f5c2
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-10-26 21:05:13 -0500

    Time: 35 ms (33.33%) | Memory: 17.1 MB (96.33%) - LeetSync

--- Trees/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ Trees/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,23 +1,9 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        
-        # aan | naa 
-        # elle
-        # el b le
-        # for a string to be palindrome all charecters must have an even count and at most one charecter can have an odd count
         if  len(s) < k: # cannot have string longer than k. k=3 aa "a"+"a"+" " is invalid
             return False
-        if len(s) == k: # we can have k palindromes
-            return True
         freq = Counter(s)
         oddCnt = 0 #count odds
         for key in freq:
             oddCnt += 1 if freq[key] %2 != 0 else 0
         return oddCnt <= k
-        
-        if oddCnt > k: # How many malindromes we have to make to account for odd charecters
-            return False
-        return True
-        
-        numPalindromes = (len(freq) - oddCnt) 
-        return numPalindromes == k
