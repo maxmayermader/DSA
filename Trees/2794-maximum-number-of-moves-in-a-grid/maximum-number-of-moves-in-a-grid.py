c68c64cdfd8bddd5c7854efa3678166b52f43751
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-11-01 12:31:52 -0500

    Time: 114 ms (70.56%) | Memory: 36.6 MB (34.44%) - LeetSync

--- Trees/2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
+++ Trees/2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
@@ -1,9 +1,7 @@
 class Solution:
-    # The three possible directions for the next column
-    dirs = [-1, 0, 1]
-    
     def maxMoves(self, grid):
         numRows, numCols = len(grid), len(grid[0])
+        dirs = [-1, 0, 1]   # The three possible directions for the next column
         q = deque()
         visited = set()
         
@@ -16,21 +14,21 @@ class Solution:
         
         while q:
             queueSize = len(q)
-            for _ in range(queueSize):
-                currRow, currCol, moveCount = q.popleft()
-                maxMoves = max(maxMoves, moveCount)
+            # for _ in range(queueSize):
+            currRow, currCol, moveCount = q.popleft()
+            maxMoves = max(maxMoves, moveCount)
+            
+            for direction in dirs:
+                # Next cell after the move
+                newRow, newCol = currRow + direction, currCol + 1
                 
-                for direction in self.dirs:
-                    # Next cell after the move
-                    newRow, newCol = currRow + direction, currCol + 1
+                # Check if next move is valid and unvisited
+                if (0 <= newRow < numRows and 
+                    0 <= newCol < numCols and 
+                    (newRow, newCol) not in visited and 
+                    grid[currRow][currCol] < grid[newRow][newCol]):
                     
-                    # Check if next move is valid and unvisited
-                    if (0 <= newRow < numRows and 
-                        0 <= newCol < numCols and 
-                        (newRow, newCol) not in visited and 
-                        grid[currRow][currCol] < grid[newRow][newCol]):
-                        
-                        visited.add((newRow, newCol))
-                        q.append((newRow, newCol, moveCount + 1))
+                    visited.add((newRow, newCol))
+                    q.append((newRow, newCol, moveCount + 1))
                         
         return maxMoves
