a828ce9a3926254ea03dc471676354e44b713ad1
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-11-06 19:06:36 -0600

    Time: 0 ms (100.00%) | Memory: 16.7 MB (29.62%) - LeetSync

--- Trees/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ Trees/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,28 +1,38 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        phone = {
+        # If the input is empty, immediately return an empty answer array
+        if len(digits) == 0:
+            return []
+
+        # Map all the digits to their corresponding letters
+        letters = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
             "5": "jkl",
             "6": "mno",
-            "7": "qprs",
+            "7": "pqrs",
             "8": "tuv",
             "9": "wxyz",
         }
 
-        res = []
-        def dfs(i, nums):
-            if i == len(digits):
-                res.append(nums)
-                return
-            
-            for c in phone[digits[i]]:
-                nums += c
-                dfs(i+1, nums)
-                nums = nums[0:-1]
+        def backtrack(index, path):
+            # If the path is the same length as digits, we have a complete combination
+            if len(path) == len(digits):
+                combinations.append("".join(path))
+                return  # Backtrack
 
-        if not digits:
-            return []
-        dfs(0, "")
-        return res 
+            # Get the letters that the current digit maps to, and loop through them
+            possible_letters = letters[digits[index]]
+            for letter in possible_letters:
+                # Add the letter to our current path
+                path.append(letter)
+                # Move on to the next digit
+                backtrack(index + 1, path)
+                # Backtrack by removing the letter before moving onto the next
+                path.pop()
+
+        # Initiate backtracking with an empty path and starting index of 0
+        combinations = []
+        backtrack(0, [])
+        return combinations
