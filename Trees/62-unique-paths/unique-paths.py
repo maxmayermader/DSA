9f5b3ce05b5855d95b0dfe5b9743969810db6cfb
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-11-14 16:52:44 -0600

    Time: 0 ms (100.00%) | Memory: 16.7 MB (39.56%) - LeetSync

--- Trees/62-unique-paths/unique-paths.py
+++ Trees/62-unique-paths/unique-paths.py
@@ -1,16 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = {(m-1, n-1) : 1} # (r, c) : move count
-        directions = [[0, -1], [-1, 0]]
-
-        for r in range(m-1, -1, -1):
-            for c in range(n-1, -1, -1):
-                for dr, dc in directions:
-                    newr, newc = r + dr, c + dc
-                    if newr >= 0 and newc >= 0:
-                        if (newr, newc) in dp:
-                            dp[(newr, newc)] +=  dp.get((r,c), 0)
-                        else:
-                            dp[(newr, newc)] =  dp.get((r,c), 0)
-
-        return dp[(0,0)]
+        dp = [[1] * n for _ in range(m)]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
