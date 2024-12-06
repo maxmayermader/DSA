ecee940607a04f98edae9caa0cecad34ee760e47
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-12-06 15:06:03 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.78%) - LeetSync

--- Trees/412-fizz-buzz/fizz-buzz.py
+++ Trees/412-fizz-buzz/fizz-buzz.py
@@ -3,11 +3,12 @@ class Solution:
         res = []
         for i in range(1, n+1):
             word = ""
-            if i % 3 == 0:
+            divby3 = i % 3 == 0
+            if divby3:
                 word += "Fizz"
             if i % 5 == 0:
                 word += "Buzz"
-            elif i % 3 != 0:
+            elif not divby3:
                 word += str(i)
             res.append(word)
 
