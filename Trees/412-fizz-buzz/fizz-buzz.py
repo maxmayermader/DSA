b9486871dd6f0886b5adbcccb763e9058e28895d
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2024-12-06 15:10:21 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (16.55%) - LeetSync

--- Trees/412-fizz-buzz/fizz-buzz.py
+++ Trees/412-fizz-buzz/fizz-buzz.py
@@ -1,16 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        res = []
+        res = [""]*n
         for i in range(1, n+1):
-            word = ""
             divby3 = i % 3 == 0
             if divby3:
-                word += "Fizz"
+                res[i-1] += "Fizz"
             if i % 5 == 0:
-                word += "Buzz"
+                res[i-1]  += "Buzz"
             elif not divby3:
-                word += str(i)
-            res.append(word)
+                res[i-1]  += str(i)
+            # res.append(word)
 
         return res
-        
