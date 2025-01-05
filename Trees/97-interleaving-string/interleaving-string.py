fd8ab532a0de9d4bf5e2a963be8407a8940741c2
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2025-01-05 17:26:40 -0600

    Time: 47 ms (35.24%) | Memory: 18.3 MB (18.08%) - LeetSync

--- Trees/97-interleaving-string/interleaving-string.py
+++ Trees/97-interleaving-string/interleaving-string.py
@@ -1,17 +1,28 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
-            return False
+        # if len(s1)+len(s2)<len(s3):
+        #     return False
+        dp = [[-1 for _ in range(len(s2)+1)]for _ in range(len(s1)+1)]
+        def helper(i1, i2, i3):
+            if i3==len(s3):
+                if i1==len(s1) and i2==len(s2):
+                    return True
+                return False
+            if i1<len(s1) and i2<len(s2):
+                if dp[i1][i2]!=-1:
+                    return dp[i1][i2]
+            if i1<len(s1) and s1[i1]==s3[i3]:
+                if helper(i1+1,i2,i3+1):
+                    dp[i1][i2] = True
+                    return dp[i1][i2]
+            if i2<len(s2) and s2[i2]==s3[i3]:
+                if helper(i1,i2+1,i3+1):
+                    dp[i1][i2] = True
+                    return dp[i1][i2]
+                
+            dp[i1][i2] = False
+            return dp[i1][i2]
+            
 
-        N, M = len(s1), len(s2)
-        dp = [[False for j in range(M+1)] for i in range(N+1)]
-        dp[N][M] = True
-
-        for i in range(N, -1, -1):
-            for j in range(M, -1, -1):
-                if i < N and s1[i] == s3[i+j] and dp[i+1][j]:
-                    dp[i][j] = True
-                if j < M and s2[j] == s3[i+j] and dp[i][j+1]:
-                    dp[i][j] = True
-
-        return dp[0][0]
+        return helper(0,0,0)
+        
