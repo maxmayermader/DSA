5ef0ca71aecd1ef3d0b525701de60e007fabfa9c
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2025-01-27 16:41:42 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (32.28%) - LeetSync

--- Trees/74-search-a-2d-matrix/search-a-2d-matrix.py
+++ Trees/74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,10 +1,7 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        R = len(matrix)
-        C = len(matrix[0])
-        
-        lr = 0
-        rr = R-1
+        R, C = len(matrix), len(matrix[0])        
+        lr, rr = 0, R-1
         
         while lr <= rr:
             mr = lr + ((rr - lr) // 2)
@@ -13,10 +10,8 @@ class Solution:
             elif matrix[mr][0] > target:
                 rr = mr - 1
             else:
-                lr = mr + 1
-        print(mr)   
-        l = 0
-        r = C - 1
+                lr = mr + 1 
+        l,r = 0, C - 1
         
         while l <= r:
             m = l + ((r - l) // 2)
