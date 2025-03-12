457bc85a0df670887cbf34ded56e43a2312517b5
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2025-03-12 17:17:18 -0500

    Time: 11 ms (72.76%) | Memory: 18 MB (33.53%) - LeetSync

--- Trees/242-valid-anagram/valid-anagram.py
+++ Trees/242-valid-anagram/valid-anagram.py
@@ -2,11 +2,10 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
         hm1, hm2 = {}, {}
 
         for i in range(len(s)):
             hm1[s[i]] = 1 + hm1.get(s[i], 0)
             hm2[t[i]] = 1 + hm2.get(t[i], 0)
-        res = hm1 == hm2
-        return res
+
+        return hm1 == hm2
