4860b4be8a026259298aa5a782dcf035776e47b9
Max Mayer-Mader <119761775+maxmayermader@users.noreply.github.com>
2025-03-12 15:25:25 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (11.45%) - LeetSync

--- Trees/1-two-sum/two-sum.py
+++ Trees/1-two-sum/two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hm = {}
-        for i in range(len(nums)):
-            if nums[i] in hm:
-                return [hm[nums[i]], i]
+        for i, num in enumerate(nums):
+            if target - num in hm:
+                return [hm[target - num], i]
             else:
-                hm[target-nums[i]] = i
+                hm[num] = i
